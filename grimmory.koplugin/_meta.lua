08732191347aace2529d169652877af3bfe275d2
James Ward <james@notjam.es>
2026-05-12 23:06:50 -0400

    chore: add repository to meta

--- grimmory.koplugin/_meta.lua
+++ grimmory.koplugin/_meta.lua
@@ -4,4 +4,5 @@ return {
     fullname = _("Grimmory Sync"),
     description = _([[Synchronize reading info with Grimmory.]]),
     version = "0.0.0-snapshot",
+    repository = "imnotjames/grimmory"
 }
